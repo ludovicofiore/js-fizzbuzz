033f96ccfd50403d31489886283577dac0bd3448
ludoff <ludovicofiore25@gmail.com>
2024-06-06 16:58:45 +0200

    aggiunta fizz, buzz, fizzbuzz per i relativi multipli in console log

--- js/main.js
+++ js/main.js
@@ -3,8 +3,32 @@
 
 // scrivere ciclo con numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
-    console.log(i);
+
+    // aggiungere al ciclo le condizioni con if etc.
+
+    // per multipli di 3 e di 5 scrivo FizzBuzz
+    if ((i % 3 === 0) && (i % 5 === 0)) {
+        let fizzbuzz = "FizzBuzz";
+       
+        console.log(fizzbuzz);
+
+        // per multipli di 5 scrivo buzz
+    } else if (i % 5 === 0) {
+        let buzz = "Buzz";
+
+        console.log(buzz);
+
+        // per multipli di 3 scrivo fizz
+    } else if (i % 3 === 0) {
+        let fizz = "Fizz";
+
+        console.log(fizz);
+
+        // altrimenti numero normale
+    } else {
+        console.log(i);
+    }
+    
 }
 
 
-// aggiungere al ciclo le condizioni con if etc.
